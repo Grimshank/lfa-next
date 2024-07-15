8735d40bd7dfc48e3f87f49fc1867784828ba9ed
vy7448 <vy7448@soe-ge.com>
2024-07-15 15:07:21 -0500

    try making the background black on the first title

--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,7 @@ export default async function Home() {
       }
 
       <div className="space-y-1">
-        <h2 className="z-10 text-3xl font-bold text-center text-transparent duration-1000 bg-white cursor-default text-stroke animate-title sm:text-5xl md:text-6xl whitespace-nowrap bg-clip-text">Join the waitlist for my</h2>
+        <h2 className="z-10 text-3xl font-bold text-center text-transparent duration-1000 bg-black cursor-default text-stroke animate-title sm:text-5xl md:text-6xl whitespace-nowrap bg-clip-text">Join the waitlist for my</h2>
         <h1 className="z-10 tet-4xl font-bold text-center text-transparent duration-1000 bg-white cursor-default sm:text-6xl md:text-7xl whitespace-nowrap bg-clip-text bg-gradient-to-r from-purple-300 to-purple-800 animate-fade-in-3">Newsletter</h1>
       </div>
     </main>
