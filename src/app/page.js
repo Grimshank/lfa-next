de4539df47e23d8cbb30c244d28dd68e812dd2e4
vy7448 <vy7448@soe-ge.com>
2024-07-15 15:05:19 -0500

    try animating title, commented out the art work images list for now

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,9 @@ export default async function Home() {
   const works = await getWorks();
 
   return (
-    <main className="bg-[#ffffff] flex flex-col items-center justify-center p-10 min-h-screen">
+    <main className="bg-white flex flex-col items-center justify-center p-10 min-h-screen">
       {
+        /*
         works.map(work => {
           console.log(`WORK: ${JSON.stringify(work.image)}`);
           return (
@@ -17,12 +18,12 @@ export default async function Home() {
             </div>
           )
         })
+        */
       }
-      <Button>Our Cool Button</Button>
 
-      <div>
-        <h2>Join the waitlist for my</h2>
-        <h1>Newsletter</h1>
+      <div className="space-y-1">
+        <h2 className="z-10 text-3xl font-bold text-center text-transparent duration-1000 bg-white cursor-default text-stroke animate-title sm:text-5xl md:text-6xl whitespace-nowrap bg-clip-text">Join the waitlist for my</h2>
+        <h1 className="z-10 tet-4xl font-bold text-center text-transparent duration-1000 bg-white cursor-default sm:text-6xl md:text-7xl whitespace-nowrap bg-clip-text bg-gradient-to-r from-purple-300 to-purple-800 animate-fade-in-3">Newsletter</h1>
       </div>
     </main>
   );
