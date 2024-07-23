e614a58fa0c35e0aa5a8374d3e381c2000dbc105
vy7448 <vy7448@soe-ge.com>
2024-07-23 14:34:08 -0500

    small changes

--- src/components/nav/LinksOverlay.js
+++ src/components/nav/LinksOverlay.js
@@ -1,6 +1,6 @@
 const LinksOverlay = () => {
   return (
-    <nav className="fixed right-4 top-4 z-40 h-[calc(100vh - 32px)] w-[calc(100% - 32px)] overflow-hidden">
+    <nav className="fixed right-4 top-4 z-40 h-[calc(100vh_-_32px)] w-[calc(100%_-_32px)] overflow-hidden">
       {
         /*
         <Logo />
