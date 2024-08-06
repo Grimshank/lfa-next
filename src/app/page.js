a912cc6a53f674a7ffb99dffbf1c6ad46c0a4e6d
vy7448 <vy7448@soe-ge.com>
2024-08-06 08:56:07 -0500

    trying to get the intermediate text above the carousel to look right on iPhone

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,9 @@ export default async function Home() {
   return (
     <main className="bg-white flex flex-col items-center justify-center min-h-screen">
       <ZoomingImageHero/>
-      <AnotherOne works={works}/>
+      <h1 className="text-3xl font-medium m-auto text-center pt-14" style={{fontFamily: 'mrs-eaves-roman-small-caps'}}>
+        Welcome to LeCrone Fine Art, home of artist and instructor Sean P. LeCrone.
+      </h1>
       <AboutSection/>
       <Footer/>
       {/*
