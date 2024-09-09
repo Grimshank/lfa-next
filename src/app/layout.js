348b50595d05d887987fb685cf81aa5a845b4d44
vy7448 <vy7448@soe-ge.com>
2024-09-09 16:01:25 -0500

    import vercel analytics correctly

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from '@/components/nav/Nav';
-import { Analytics } from '@vercel/analytics';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
