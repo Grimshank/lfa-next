f390ba81f643e57974daa898eedddccdbce93639
vy7448 <vy7448@soe-ge.com>
2024-09-09 15:33:04 -0500

    change png to jpg reference for portrait image

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from "next/link";
 
-import portrait from '../../public/portrait.png';
+import portrait from '../../public/portrait.jpg';
 
 function AboutSection() {
   return (
