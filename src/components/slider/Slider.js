e7701738a61cdcc44f8d656105bef7636817fdfd
vy7448 <vy7448@soe-ge.com>
2024-09-11 23:50:19 -0500

    add custom analytics event when slider image is clicked

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -4,8 +4,13 @@ import {Swiper, SwiperSlide} from 'swiper/react';
 import { Navigation, Pagination, A11y } from 'swiper/modules';
 
 import 'swiper/swiper-bundle.css';
+import {track} from "@vercel/analytics";
 
 function Slider({ works}) {
+  function handleWorkClicked(work) {
+    track('Slider Engagement', {title: work.title});
+  }
+
   return (
     <Swiper
       modules={[Navigation, Pagination]}
@@ -19,7 +24,10 @@ function Slider({ works}) {
         works.map((work, index) => (
           <SwiperSlide key={index}>
             <div className="flex flex-col justify-center items-center h-[175px] md:h-[500px]">
-              <img className="h-[175px] md:h-[400px]" src={work.src} alt={work.title} />
+              <img
+                onClick={() => handleWorkClicked(work)}
+                className="h-[175px] md:h-[400px]" src={work.src} alt={work.title}
+              />
             </div>
           </SwiperSlide>
         ))
