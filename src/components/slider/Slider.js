ed39fb02a395339bdc98fb200e091269d57f5ec2
vy7448 <vy7448@soe-ge.com>
2024-09-15 07:24:01 -0500

    add links to individual works pages to images in the slider

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -2,11 +2,12 @@
 
 import {Swiper, SwiperSlide} from 'swiper/react';
 import { Navigation, Pagination, A11y } from 'swiper/modules';
+import Link from 'next/link';
+import {track} from '@vercel/analytics';
 
 import 'swiper/swiper-bundle.css';
-import {track} from "@vercel/analytics";
 
-function Slider({ works}) {
+function Slider({ works }) {
   function handleWorkClicked(work) {
     track('Slider Engagement', {title: work.title});
   }
@@ -24,10 +25,12 @@ function Slider({ works}) {
         works.map((work, index) => (
           <SwiperSlide key={index}>
             <div className="flex flex-col justify-center items-center h-[175px] md:h-[500px]">
-              <img
-                onClick={() => handleWorkClicked(work)}
-                className="h-[175px] md:h-[400px]" src={work.src} alt={work.title}
-              />
+              <Link href={`/works/${work.id}`}>
+                <img
+                  onClick={() => handleWorkClicked(work)}
+                  className="h-[175px] md:h-[400px]" src={work.src} alt={work.title}
+                />
+              </Link>
             </div>
           </SwiperSlide>
         ))
