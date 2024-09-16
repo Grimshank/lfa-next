970f60cf5db1e51ab488465c3ca04dade74be265
vy7448 <vy7448@soe-ge.com>
2024-09-16 08:35:30 -0500

    clean up and add some stuff to faq page

--- src/app/faq/page.js
+++ src/app/faq/page.js
@@ -25,7 +25,8 @@ export default function About() {
           <p className="italic font-bold mb-[10px]">Does Sean accept commissions?</p>
           <p>
             Yes, Sean accepts commissions. Sean will work closely with his clients at each step of
-            the processes from preliminary sketch to final framing. If you would like to inquire about a
+            the processes from preliminary sketch to final framing. If you would like to inquire
+            about a
             commission you can click the button below.
           </p>
           <div className="mt-4">
@@ -44,7 +45,8 @@ export default function About() {
         >
           <p className="italic font-bold mb-[10px]">What colors does Sean use on his palette?</p>
           <p>
-            In his classes Sean stresses the importance of placing colors on the palette in a certain
+            In his classes Sean stresses the importance of placing colors on the palette in a
+            certain
             arrangement, clockwise starting from the bottom left corner of the palette:
           </p>
           <ul className="mt-2">
@@ -63,6 +65,7 @@ export default function About() {
             <li>Cadmium Yellow Lemon</li>
             <li>Titanium White</li>
           </ul>
+          <p className="mt-2">Sean uses the same palette for both oil and acrylic works.</p>
         </motion.div>
 
         <motion.div
@@ -77,20 +80,24 @@ export default function About() {
           <div className="flex flex-row">
             <div>
               <a href="https://artgrouparkansas.com">
-                <Image src={aggLogo} className="max-w-[100px] max-h-[100px]" width={200} height={200} alt='agg_logo'/>
+                <Image src={aggLogo} className="max-w-[100px] max-h-[100px]" width={200}
+                       height={200} alt='agg_logo'/>
               </a>
             </div>
             <div className="grid grid-cols-1 ml-2">
               <div>
                 <p>
                   Sean primarily teaches at the Art Group Gallery. The gallery is located at
-                  Pleasant Ridge Town Center, 11525 Cantrell Road in Little Rock, Arkansas. Currently
+                  Pleasant Ridge Town Center, 11525 Cantrell Road in Little Rock, Arkansas.
+                  Currently
                   there is only one class that meets from 9am-11am every Wednesday morning.
                 </p>
               </div>
               <div className="mt-2">
                 <p>
-                  He is also a volunteer instructor for the Life Quest
+                  He is also a volunteer instructor for tor LifeQuest of Arkansas.  You can find out
+                  more about LifeQuest and if Sean is scheduled to teach in upcoming classes by visiting
+                  the <a href="https://lifequestofarkansas.org">LifeQuest of Arkansas</a> website.
                 </p>
               </div>
             </div>
@@ -109,14 +116,44 @@ export default function About() {
           <div className="flex flex-row">
             <div>
               <a href="https://artgrouparkansas.com">
-                <Image src={aggLogo} className="max-w-[100px] max-h-[100px]" width={200} height={200} alt='agg_logo'/>
+                <Image src={aggLogo} className="max-w-[100px] max-h-[100px]" width={200}
+                       height={200} alt='agg_logo'/>
               </a>
             </div>
             <p className="ml-2">
-            Sean currently shows his work at the Art Group Gallery. The gallery is located at
-              Pleasant Ridge Town Center, 11525 Cantrell Road in Little Rock, Arkansas. The hours are
+              Sean currently shows his work at the Art Group Gallery. The gallery is located at
+              Pleasant Ridge Town Center, 11525 Cantrell Road in Little Rock, Arkansas. The hours
+              are
               Monday through Saturday from 11am-5pm and Sunday from 1pm-5pm. You can see available
-              works by Sean and other artists at <a href="https://artgrouparkansas.com">www.artgrouparkansas.com</a>.
+              works by Sean and other artists at <a
+              href="https://artgrouparkansas.com">www.artgrouparkansas.com</a>.
+            </p>
+          </div>
+        </motion.div>
+
+        <motion.div
+          initial={{opacity: 0}}
+          whileInView={{opacity: 1}}
+          transition={{
+            duration: 2.5,
+          }}
+          className="mb-[30px]"
+        >
+          <p className="italic font-bold mb-[10px]">Does Sean ship and how is it arranged?</p>
+          <div>
+            <p className="">
+              Yes, Sean will ship paintings and prints to you at an additional cost determined at the
+              time of purchase.
+            </p>
+            <p className="mt-2">
+              When it comes to shipping, Sean makes the process as seamless as possible by working
+              closely with the customer to arrange all the details according to their needs. He begins
+              by discussing the shipping options available depending on the urgency.  We review the
+              shipping costs, delivery times, and any specific preferences the customer might have,
+              such as insurance or handling requirements. Throughout the process, Sean ensures clear
+              communication, providing updates on tracking and estimated delivery times, so the
+              customer is always informed. My goal is to tailor the shipping experience to their
+              convenience, ensuring their satisfaction from start to finish.
             </p>
           </div>
         </motion.div>
