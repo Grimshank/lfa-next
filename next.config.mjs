4742ffebf8b068b473bd1758132b4aa6fcd54716
vy7448 <vy7448@soe-ge.com>
2024-09-16 13:03:20 -0500

    make a change to image nextConfig hostname for instagram

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
      },
       {
         protocol: 'https',
-        hostname: 'scontent-hou1-1.cdninstagram.com',
+        hostname: '*.cdninstagram.com',
         port: '',
         pathname: '/**',
       },
