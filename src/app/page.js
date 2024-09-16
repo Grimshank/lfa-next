a87b180343ac282d49110072914fd5a212fa4b43
vy7448 <vy7448@soe-ge.com>
2024-09-16 13:31:49 -0500

    fix bug

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import Slider from '@/components/slider/Slider';
 import NewsletterSection from '@/components/NewsletterSections';
 import InstagramFeed from '@/components/instagram/InstagramFeed';
 
-export const Metadata = {
+export const metadata = {
   title: 'LeCrone Fine Art',
   description: 'Explore the captivating artwork of Sean LeCrone, featuring a comprehensive catalog of his paintings and artistic creations. Discover detailed information about his style, artistic journey, upcoming exhibitions, and classes.',
 }
