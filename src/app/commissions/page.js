48e392b55f433e4de79bcc0ceb6faafcea6a80e6
vy7448 <vy7448@soe-ge.com>
2024-09-23 13:37:45 -0500

    add small header to commissions page

--- src/app/commissions/page.js
+++ src/app/commissions/page.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {getWork} from '@/lib/contentful';
-import Image from 'next/image';
 import InquiryForm from '@/components/InquiryForm';
 import Footer from '@/components/Footer';
+import SmallHeader from '@/components/SmallHeader';
 
 export default async function Inquire({ params }) {
   return (
     <div
       className="w-full px-8 py-12 grid grid-cols-1 mt-20"
     >
+      <SmallHeader />
       <h1 className="text-3xl mb-4 font-medium m-auto text-center pt-14" style={{fontFamily: 'mrs-eaves-roman-small-caps'}}>
         Inquire About a Commission</h1>
       <div className="md:ml-8 mb-4">
