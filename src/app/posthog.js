1b0209ef4da743a9f548e8cc3eeeda9d2e01fa90
vy7448 <vy7448@soe-ge.com>
2024-10-02 18:00:30 -0500

    trying to get posthog server comonents to work; they work locally but not on vercel for some reason

--- src/app/posthog.js
+++ src/app/posthog.js
@@ -1,9 +1,10 @@
 import { PostHog } from 'posthog-node';
 
 export default function PostHogClient() {
-  console.log('PostHogClient()');
+  console.log(`PostHogClient(): ${process.env.POSTHOG_API_KEY}`);
   const postHogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    personalApiKey: process.env.POSTHOG_API_KEY,
   });
   postHogClient.on('error', (err) => {
     console.log('!!!!!!!!!')
